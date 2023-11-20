9e50b94739b1432497bd79f0f1499dddab592f01
cysof <cp4reallife@yahoo.com>
2023-11-20 13:59:32 -0800

    task zero fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
-            count +=1
-        print()
-        return count
-    except IndexError:
+    count = o
+    for i in range(x):
+        try:
+            print('{}'.format(my_list[i]), end='')
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        except IndexError:
+            break
         print()
         return count
