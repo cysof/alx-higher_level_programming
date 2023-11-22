672443f6f9e726468d1dec15c17b83283f434e3d
cysof <cp4reallife@yahoo.com>
2023-11-21 19:20:09 -0800

    task 1-9

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """AN empty class square."""
+    pass
+
+if __name__ == '__main__':
+    my_square = square()
+    print(type(my_square))
+    print(my_square.__dict__)
+
