2bc9a750fecd9b9b1a30c20146669bdc27ae7427
cysof <cp4reallife@yahoo.com>
2023-11-30 05:06:51 -0800

    fix task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
 class Square:
-    """AN empty class square."""
-    pass
+    """
+    This class represents a square.
 
-if __name__ == '__main__':
-    my_square = square()
-    print(type(my_square))
-    print(my_square.__dict__)
+    Attributes:
+        None at the moment, as this is an empty class definition.
+    """
+
+    pass
 
