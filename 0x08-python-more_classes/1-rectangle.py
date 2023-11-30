e3eab9f4cce853b542ee21705f46f98e48546844
cysof <cp4reallife@yahoo.com>
2023-11-29 19:29:26 -0800

    task 1-2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,38 +1,53 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
+
 
 class Rectangle:
-    """ Rectangle Class """
+    """Rectangle class."""
 
     def __init__(self, width=0, height=0):
-        """ __init__ method.
+        """__init__ method.
         Args:
-            width (ini): integer width
-            height (init: integer height
-            """
-            self.width = width
-            self.height =height
-        @property
-        """ property Decorrector """
-        def width(self):
-            """width: returns width
-            Args:
-                width (int): integer width
-            Returns:
-                Rectangle width
-            Raises:
-                TypeError: if width is not an integer
-                ValueError: if width is less 0
-            """
-            return self.__height
+            width (int): integer width
+            height (int): integer height
+        """
+        self.width = width
+        self.height = height
 
-        @width.setter
-        def width(self, width):
-            if not isintance(width, int):
-                raise TypeError("width must be an integer")
-            if width < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = width
-             @height.setter
+    @property
+    def width(self):
+        """width: returns width
+        Args:
+            width (int): integer width
+        Returns:
+            rectangle width
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width is less than 0
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """height: returns height
+        Args:
+            height (int): integer height
+        Returns:
+            rectangle height
+        Raises:
+            TypeError: if height is not an integer
+            ValueError: if height is less than 0
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @height.setter
     def height(self, height):
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
