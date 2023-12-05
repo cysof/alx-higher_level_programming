ed902e7766c3abb43c0bff59acc524f4a625a993
cysof <cp4reallife@yahoo.com>
2023-12-04 18:18:01 -0800

    task 2 redo

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 """
 MyList Module.
-Create a class that inherits properties and attributes
-from 'list'
+Create a class that inherits properties and attributes from `list`
 """
+
 
 class MyList(list):
     """
@@ -11,8 +12,9 @@ class MyList(list):
     Methods: print_sorted(self)
     """
     def __init__(self):
-        """initializing MyList"""
-        supper().__init__()
+        """initializer for MyList"""
+        super().__init__()
+
     def print_sorted(self):
         """prints the list(sorted) in ascending order
         type(int)
