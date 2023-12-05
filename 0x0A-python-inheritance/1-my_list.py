76ac5ce3f0e59344c90833db1063821eacb1cf77
cysof <cp4reallife@yahoo.com>
2023-12-04 17:57:10 -0800

    task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+MyList Module.
+Create a class that inherits properties and attributes
+from 'list'
+"""
+
+class MyList(list):
+    """
+    Inherits from list
+    Methods: print_sorted(self)
+    """
+    def __init__(self):
+        """initializing MyList"""
+        supper().__init__()
+    def print_sorted(self):
+        """prints the list(sorted) in ascending order
+        type(int)
+        """
+        print(sorted(self))
