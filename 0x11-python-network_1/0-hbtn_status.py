99dfc8783e04e07f9d33c441ac958aae985a13d9
cysof <cp4reallife@yahoo.com>
2024-03-04 22:44:58 -0800

    task 0 and 4

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" script fetches https://intranet.hbtn.io/status"""
 import urllib.request
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode(encoding='utf-8')))
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", html.decode('utf-8'))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-""" script that fetches https://intranet.hbtn.io/status using requests """
-if __name__ == '__main__':
-    import requests
 
-    response = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+"""Script that fetches https://alx-intranet.hbtn.io/status.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    content = response.text
+
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
